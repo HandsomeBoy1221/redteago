8553861ad9b8ab6043b6a9f2fef7b8dc5dea93a1
Tom <tom.zheng@redteamobile.com>
2020-08-22 17:05:24 +0800

    回归测试用例

--- page/orders_page.py
+++ page/orders_page.py
@@ -1,6 +1,6 @@
 from page.base_page import BasePage
 
-
+#全部订单页面
 class OrdersPage(BasePage):
     def open_orders(self):
         return self.step('../steps/orders_page.yaml', 'open_orders')
--- page/version_page.py
+++ page/version_page.py
@@ -1,6 +1,6 @@
 from page.base_page import BasePage
 
-
+#版本信息页面
 class VersionPage(BasePage):
     def protocol(self):
         return self.step('../steps/version_page.yaml', 'protocol')
